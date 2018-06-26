492b36146b959f5ffea6074a01d680361c79d605
Sponge Bob <michel.boucey@cybervisible.com>
2018-06-26 16:15:48 +0200

    Reading YAML file is OK

--- brutedrop.go
+++ brutedrop.go
@@ -4,34 +4,34 @@ import (
 	"fmt"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
+        "log"
 	// import "regexp"
 )
 
 type Config struct {
-	Iptables            string   //   `yaml:"iptables"`
-	IptablesChain       string   //   `yaml:"iptables_chain"`
-	Journalctl          string   //   `yaml:"journalctl"`
-	AuthorizedUsers     []string //   `yaml:"authorized_users"`
-	AuthorizedAddresses []string //   `yaml:"authorized_addresses"`
+	Iptables            string   `yaml:"Iptables"`
+	IptablesChain       string   `yaml:"IptablesChain"`
+	Journalctl          string   `yaml:"Journalctl"`
+	AuthorizedUsers     []string `yaml:"AuthorizedUsers"`
+	AuthorizedAddresses []string `yaml:"AuthorizedAddresses"`
 }
 
 func main() {
 
 	var config Config
 
-	data, rferr := ioutil.ReadFile("btedrop.conf")
+	data, err := ioutil.ReadFile("conf/brutedrop.conf")
 
-	if rferr != nil {
-		fmt.Errorf("Error reading configuration file: %v", rferr)
+	if err != nil {
+                log.Fatal(err)
 	}
 
-	umerr := yaml.UnmarshalStrict(data, &config)
+	// fmt.Println(string(data))
 
-	if umerr != nil {
-		fmt.Errorf("Configuration file errors: %v", umerr)
+	err = yaml.Unmarshal(data,&config)
+
+	if err != nil {
+                log.Fatal(err)
 	}
-
-	// fmt.Println("bruteDrop")
-	fmt.Println(config.Iptables)
 
 }
