5e1f8b7e6145257f59e6f17778dbb11a47eed3bc
root <michel.boucey@gmail.com>
2018-06-26 17:35:14 +0200

    WIP

--- brutedrop.go
+++ brutedrop.go
@@ -4,8 +4,9 @@ import (
 	"fmt"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
-        "log"
-        "os/exec"
+	"log"
+	"os/exec"
+	"strings"
 	// import "regexp"
 )
 
@@ -21,22 +22,35 @@ func main() {
 
 	var config Config
 
+	var lines []string
+
 	data, err := ioutil.ReadFile("conf/brutedrop.conf")
 
 	if err != nil {
-                log.Fatal(err)
+		log.Fatal(err)
 	}
 
-	err = yaml.Unmarshal(data,&config)
+	err = yaml.Unmarshal(data, &config)
 
 	if err != nil {
-                log.Fatal(err)
+		log.Fatal(err)
 	}
 
-        out, err := exec.Command("bash","-c","journalctl --since \"5 minutes ago\" -u sshd --no-pager | grep Failed").Output()
+	// out, err := exec.Command("bash","-c","journalctl --since \"5 minutes ago\" -u sshd --no-pager | grep Failed").Output()
+	out, err := exec.Command("bash", "-c", "journalctl --since \"5 minutes ago\" -u sshd --no-pager").Output()
 
-	if err != nil {
-                log.Fatal(err)
+	if string(out) == "" {
+		fmt.Println("Nil")
 	}
-        fmt.Print(string(out))
+
+	lines = strings.Split(string(out), "\n")
+
+	// Iterating over lines
+	for i := 0; i < len(lines); i++ {
+		// /^(\D{3}\s\d{2}\s\d{2}:\d{2}:\d{2}).*?for\s(invalid user\s|)(.+)\sfrom\s(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/
+		fmt.Print("> ")
+		fmt.Println(lines[i])
+		fmt.Println()
+
+	}
 }
