d0f5355ce5ef86754ec821700f3e810b8712f9fa
root <michel.boucey@gmail.com>
2018-06-28 16:01:18 +0200

    WIP

--- brutedrop.go
+++ brutedrop.go
@@ -11,33 +11,15 @@ import (
 	"strings"
 )
 
-type Config struct {
-	Iptables            string   `yaml:"Iptables"`
-	IptablesChain       string   `yaml:"IptablesChain"`
-	Journalctl          string   `yaml:"Journalctl"`
-	AuthorizedUsers     []string `yaml:"AuthorizedUsers"`
-	AuthorizedAddresses []string `yaml:"AuthorizedAddresses"`
-}
-
-func isElement(e string, l []string) bool {
-	for i := 0; i < len(l); i++ {
-		if l[i] == e {
-			return true
-		}
-	}
-	return false
-}
-
 func main() {
 
 	var config Config
 
 	var lines []string
 
-	// var invalidUser = regexp.MustCompile(`/^(\D{3}\s\d{2}\s\d{2}:\d{2}:\d{2}).*?for\s(invalid user\s|)(.+)\sfrom\s(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/`)
-	var invalidUser = regexp.MustCompile(`\sfor\s(invalid\suser\s|)(.+)\sfrom\s(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s`)
+	var invalidUser = regexp.MustCompile(`(\D*?\s\d{2}\s\d{2}:\d{2}:\d{2}).*?\sfor\s(invalid\suser\s|)(.+)\sfrom\s(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s`)
 
-	data, err := ioutil.ReadFile("conf/brutedrop.conf")
+	data, err := ioutil.ReadFile("/etc/brutedrop.conf")
 
 	if err != nil {
 		log.Fatal(err)
@@ -50,10 +32,13 @@ func main() {
 	}
 
 	// AuthorizedUsers and AuthorizedAddresses can't be both empty
-	// TODO ("You have to add exceptions in brutedrop white lists.")
+	if len(config.AuthorizedUsers) == 0 && len(config.AuthorizedAddresses) == 0 {
+		fmt.Println("You have to add authorized users or IP addresses in /etc/brutedrop.conf")
+		os.Exit(0)
+	}
 
-        // Get log lines of failed SSH login attempts from journalctl
-	out, err := exec.Command("bash", "-c", "journalctl --since \"43830 minutes ago\" -u sshd --no-pager | grep Failed").Output()
+	// Get log lines of failed SSH login attempts from journalctl
+	out, err := exec.Command("sh", "-c", "journalctl --since \"43830 minutes ago\" -u sshd --no-pager | grep Failed").Output()
 
 	if string(out) == "" {
 		fmt.Println("No log lines to process")
@@ -65,12 +50,39 @@ func main() {
 	// Iterating over log lines
 	for i := 0; i < len(lines); i++ {
 		if lines[i] != "" {
+
 			matches := invalidUser.FindStringSubmatch(lines[i])
-			if isElement(matches[2], config.AuthorizedUsers) {
-				fmt.Println("Authorized user " + matches[2] + "@" + matches[3] + " failed to login")
+			timestamp := "[" + matches[1] + "]"
+			origin := matches[3] + "@" + matches[4]
+
+			if isElement(matches[3], config.AuthorizedUsers) {
+				logging(config.LogPath, timestamp + " Authorized user " + origin + " failed to login")
 			} else if !isElement(matches[3], config.AuthorizedAddresses) {
-				fmt.Println("Unauthorized user " + matches[2] + "@" + matches[3] + " failed to login")
+				logging(config.LogPath, timestamp + " Unauthorized user " + origin + " failed to login")
 			}
 		}
 	}
 }
+
+type Config struct {
+	Iptables            string   `yaml:"Iptables"`
+	IptablesChain       string   `yaml:"IptablesChain"`
+	Journalctl          string   `yaml:"Journalctl"`
+	LogPath             string   `yaml:"LogPath"`
+	AuthorizedUsers     []string `yaml:"AuthorizedUsers"`
+	AuthorizedAddresses []string `yaml:"AuthorizedAddresses"`
+}
+
+func isElement (e string, l []string) bool {
+	for i := 0; i < len(l); i++ {
+		if l[i] == e {
+			return true
+		}
+	}
+	return false
+}
+
+func logging (p string, s string) {
+	exec.Command("sh", "-c", "echo " + s + " >> " + p).Run()
+}
+
